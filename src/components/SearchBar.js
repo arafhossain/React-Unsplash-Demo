16171d94651ac4491c4a40fae581769bc4f5622c
Araf Hossain <arafh13@gmail.com>
2019-08-27 16:04:37 -0400

    Initialized State

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    inputChange(event){
-        console.log(event.target.value)
-    }
+    state = { term: '' }
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
-                    <label>Image Search</label>
-                    <input type="text" onChange={this.inputChange}/>
+                        <label>Image Search</label>
+                        <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })} />
                     </div>
                 </form>
             </div>
