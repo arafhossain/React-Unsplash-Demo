e743978c46c2170a4b070c2b0bf714cf624849f6
Araf Hossain <arafh13@gmail.com>
2019-08-28 11:09:09 -0400

    Bound onSearchSubmit to state

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,22 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    async onSearchSubmit(term){
+    state = {images: [ ]}
+     onSearchSubmit = async (term) => {
         let response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID af704aef45d9d5bf11ac281a5a7437fa259d7361411d4f4f0fca68d9b94326b1'
             }
         })
-        console.log(response.data.results);
+        this.setState({images: response.data.results})
     }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images.
             </div>
         );
     }
