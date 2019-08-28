24953adde7ca179f0e76f3404a111158d43b8435
Araf Hossain <arafh13@gmail.com>
2019-08-27 21:09:50 -0400

    Added Axios Request

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import SearchBar from './SearchBar'
+import axios from 'axios';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit = (term) => {
-        console.log(term);
+    async onSearchSubmit(term){
+        let response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID af704aef45d9d5bf11ac281a5a7437fa259d7361411d4f4f0fca68d9b94326b1'
+            }
+        })
+        console.log(response.data.results);
     }
 
     render() {
