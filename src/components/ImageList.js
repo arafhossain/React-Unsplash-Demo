3ff67049dbf5d6ab05167957708c1a64fbb27413
Araf Hossain <arafh13@gmail.com>
2019-08-28 15:37:16 -0400

    Added Alt tag

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 let ImageList = (props) => {
-    console.log(props.images);
     let images = props.images.map((image) => {
-        return <img key={image.id} src={image.urls.small} />
+        return <img key={image.id} alt={image.description} src={image.urls.small} />
     })
     return <div>{images}</div>;
 };
