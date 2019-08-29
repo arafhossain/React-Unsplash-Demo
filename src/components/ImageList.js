329f95a2c0d3a082fcfe73e58e82fab7195abc72
Araf Hossain <arafh13@gmail.com>
2019-08-29 10:07:01 -0400

    Created image CSS styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ImageList.css';
 
 let ImageList = (props) => {
     let images = props.images.map((image) => {
         return <img key={image.id} alt={image.description} src={image.urls.small} />
     })
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
